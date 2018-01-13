107bbb188648f52e181402034bae546cdffcc04e
Chris <Chris@CHRISNOTEBOOK>
2018-01-13 10:52:32 -0700

    Add project files.

---
+++ ChiliCookoff2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChiliCookoff2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ChiliCookoff2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChiliCookoff2.WebApiApplication" Language="C#" %>
